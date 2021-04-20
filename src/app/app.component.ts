8f254247e11f877b6176f88e264decf60977ceca
Vepathakpawan <pawanved007@gmail.com>
2021-04-20 08:42:37 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APIcallsubObj';
+  apiUrl = 'http://jsonplaceholder.typicode.com/users';
+  apiData: any;
+constructor(private http: HttpClient){
+}
+ngOnInit(){
+  this.http.get(this.apiUrl).subscribe((data)=>{
+  console.warn(data)
+  this.apiData=data;
+  })
+  }
 }
